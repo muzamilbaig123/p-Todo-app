749aacdd71aa056cab3f1b1b695b1233613aa4b7
= <amberoortiz@gmail.com>
2024-11-07 11:26:00 -0800

    todo is compelete but rtx and firebase not added

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import Footer from "@/components/footer"
+import MainSection from "@/components/main"
+import Navbar from "@/components/navbar"
+
+export default function App () {
+
+  return (
+    <>
+      <Navbar />
+
+      <MainSection />
+
+      <Footer />
+    </>
+  )
+}
